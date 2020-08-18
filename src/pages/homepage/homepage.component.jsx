5fec01d6b5ae155e756c2a62dd3027bc8a0252fb
Ricky Patnaik <ricky.patnaik@gmail.com>
2020-08-19 00:11:26 +0530

    homepage menu item style finished

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../component/direcctory/directory.component';
+import './homepage.styles.scss';
+
+const HomePage = (props) => {
+	return (
+		<div className='homepage'>
+			<Directory />
+		</div>
+	);
+};
+
+export default HomePage;
